4a84fa2907d8035028b26df17ab674332151c07d
spac3y <73653851+Spac3y@users.noreply.github.com>
2022-10-12 22:20:52 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 from lib.PyAutoClassroom import Student
 
-Student = Student('vlad.gavanescu@cnodobescu.ro', 'Vlad2020', True)
+Student = Student('usr, 'pass', True)
 Student.login()
-Student.enter_course('Limba Romana' )
+Student.enter_course('Class' )
 Student.__exit__()
